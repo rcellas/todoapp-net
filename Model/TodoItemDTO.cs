a7d4afd0535c1b6369ea6a2f7f5d92742a1dec5f
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-02-22 21:36:10 +0100

    add dto

--- Model/TodoItemDTO.cs
+++ Model/TodoItemDTO.cs
@@ -1,9 +1,8 @@
 namespace WebApplication1.Model;
 
-public class TodoItem
+public class TodoItemDTO
 {
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
 }
