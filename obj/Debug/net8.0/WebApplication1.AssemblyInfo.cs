d903a031a90fafdd194a1f7ce356ed7a9ab3ebd2
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-03-06 10:39:01 +0100

    add solution for CORS problem

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb3183f0763e1986daab5a1467895552542981c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d4afd0535c1b6369ea6a2f7f5d92742a1dec5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
